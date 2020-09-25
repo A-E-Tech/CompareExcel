406a6190743dadfd8aab532999e913a07f94f41c
tornado36 <514019326@qq.com>
2020-09-25 23:50:51 +0800

    feat(first-line): check in the first line of code

---
+++ CompareExcel/Program.cs
@@ -0,0 +1,25 @@
+﻿using CompareExcelCore;
+using System;
+
+namespace CompareExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string superSetName = "SuperSet.xlsx"; //should be user input
+            string supperSetSheet = "MV"; //should be user input
+            string childSetName = "ChildSet.xlsx"; //should be user input
+            string childSetSheet = "MV Config"; //should be user input
+            string sErr = "";
+            OfficeHelper office = new OfficeHelper();
+            sErr = office.CompareFiles(superSetName, supperSetSheet, childSetName, childSetSheet);
+            if (sErr == "")
+            {
+                sErr += "Mission Completed!";
+            }
+            Console.WriteLine(sErr);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CompareExcelCore/Class/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompareExcelCore.Class
+{
+    public class Tag
+    {
+        public int RowIndex { get; set; }
+        public int ColumnIndex { get; set; }
+        public string MV { get; set; }
+        public string CV { get; set; }
+        public string? Value { get; set; }
+    }
+}
